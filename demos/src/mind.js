2bd5ed9f2c31f92c63a34243d3f64704fa749d1d
TomHuangCN <418466938@qq.com>
2018-11-02 16:28:45 +0800

    publish 1.1.1

--- demos/src/mind.js
+++ demos/src/mind.js
@@ -8,4 +8,10 @@ const mind = new Editor.Mind({
     height: window.innerHeight - 38
   }
 });
+const minimap = new Editor.Minimap({
+  container: 'minimap',
+  height: 120,
+  width: 200
+});
 editor.add(mind);
+editor.add(minimap);
